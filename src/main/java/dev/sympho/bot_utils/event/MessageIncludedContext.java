ef50bbfdebf8b4027e5cbad4b1c7d2874bee5084
Thiago Marback <tmarback@sympho.dev>
2023-10-25 15:46:20 +0900

    Fix a typo

--- src/main/java/dev/sympho/bot_utils/event/MessageIncludedContext.java
+++ src/main/java/dev/sympho/bot_utils/event/MessageIncludedContext.java
@@ -3,7 +3,7 @@ package dev.sympho.bot_utils.event;
 import discord4j.core.object.entity.Message;
 
 /**
- * The context of an event that is related to a message and <b>analwaysd</b> includes the 
+ * The context of an event that is related to a message and <b>always</b> includes the 
  * message data as part of the event payload.
  *
  * @version 1.0
